6b64d7d8bff58b68301a4c3a4486022e91022afc
BHUMIKA KADU✨ <kadubhumika2468@gmail.com>
2026-06-20 16:02:30 +0530

    Use env var for DB connection, trim requirements

--- database.py
+++ database.py
@@ -1,19 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://postgres:2468@localhost:5432/mazeblitz_db"
+)
 
-DATABASE_URL = "postgresql://postgres:2468@localhost:5432/mazeblitz_db"
-
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
-
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
 
 Base = declarative_base()
